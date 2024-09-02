b8f97f8bff569fe89d461ac82409d28debab6da5
RobertMili <robertmilicevic3869@gmail.com>
2024-09-02 11:50:25 +0200

    add on blog

--- src/components/VideoWithFlyingInText/VideoWithFlyInText.js
+++ src/components/VideoWithFlyingInText/VideoWithFlyInText.js
@@ -1,24 +1,83 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './VideoWithFlyInText.css'; 
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import "./VideoWithFlyInText.css";
 
-const MediaWithFlyInText = ({ mediaSrc, text, isVideo, width, height, textColor = 'white', textSize = '2em', textPosition = 'center', textAlignHorizontal = 'center' }) => {
+const MediaWithFlyInText = ({
+  mediaSrc,
+  text,
+  isVideo,
+  width,
+  height,
+  textColor = "white",
+  textSize = "2em",
+  textPosition = "center",
+  textAlignHorizontal = "center",
+}) => {
+  const [textLeft, setTextLeft] = useState('50%');
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (textAlignHorizontal === 'left') {
+        if (window.innerWidth <= 768) {
+          setTextLeft('35%');
+        } else if (window.innerWidth <= 1100) {
+          setTextLeft('31%');
+        } else {
+          setTextLeft('19%');
+        }
+      } else if (textAlignHorizontal === 'right') {
+        setTextLeft('90%');
+      } else {
+        setTextLeft('50%');
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Call once to set initial size
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [textAlignHorizontal]);
+
   const textStyle = {
     color: textColor,
     fontSize: textSize,
-    '--text-top': textPosition === 'top' ? '10%' : textPosition === 'bottom' ? '90%' : '53%',
-    '--text-left': textAlignHorizontal === 'left' ? (window.innerWidth <= 768 ? '35%' : '19%') : textAlignHorizontal === 'right' ? '90%' : '50%',
-transform: 'translate(-50%, -50%)',
+    "--text-top":
+      textPosition === "top"
+        ? "10%"
+        : textPosition === "bottom"
+        ? "90%"
+        : "53%",
+    "--text-left": textLeft,
+    transform: "translate(-50%, -50%)",
   };
 
   return (
-    <div className="media-container" style={{ width, height, position: 'relative' }}>
+    <div
+      className="media-container"
+      style={{ width, height, position: "relative" }}
+    >
       {isVideo ? (
-        <video className='hero-media' src={mediaSrc} autoPlay loop muted style={{ width: '100%', height: '100%' }} />
+        <video
+          className="hero-media"
+          src={mediaSrc}
+          autoPlay
+          loop
+          muted
+          style={{ width: "100%", height: "100%" }}
+        />
       ) : (
-        <img className='hero-media' src={mediaSrc} alt={text} style={{ width: '100%', height: '100%' }} />
+        <img
+          className="hero-media"
+          src={mediaSrc}
+          alt={text}
+          style={{ width: "100%", height: "100%" }}
+        />
       )}
-      <h2 className="fly-in-text" style={textStyle}>{text}</h2>
+      <h2 className="fly-in-text" style={textStyle}>
+        {text}
+      </h2>
     </div>
   );
 };
@@ -31,8 +90,8 @@ MediaWithFlyInText.propTypes = {
   height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   textColor: PropTypes.string,
   textSize: PropTypes.string,
-  textPosition: PropTypes.oneOf(['top', 'center', 'bottom']),
-  textAlignHorizontal: PropTypes.oneOf(['left', 'center', 'right']),
+  textPosition: PropTypes.oneOf(["top", "center", "bottom"]),
+  textAlignHorizontal: PropTypes.oneOf(["left", "center", "right"]),
 };
 
 export default MediaWithFlyInText;
