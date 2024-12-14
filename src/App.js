250707ac0630985fe9ff80b16c6a7a06820d1ab1
RobertMili <robertmilicevic3869@gmail.com>
2024-12-14 12:09:03 +0100

    add to broke a app.js because customer want to deploy later

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React, { useState } from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import AboutOss from './components/pages/AboutOss/AboutOss';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Home from "./components/pages/Home";
-import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
-import Villkor from "./components/Villkor/Villkor";
-import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
+// import React, { useState } from "react";
+// import Navbar from "./components/Navbar/Navbar";
+// import Footer from "./components/Footer/Footer";
+// import AboutOss from './components/pages/AboutOss/AboutOss';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import "./App.css";
+// import Home from "./components/pages/Home";
+// import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
+// import Villkor from "./components/Villkor/Villkor";
+// import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 
-function App() {
-  const [isAdmin, setIsAdmin] = useState(false);
+// function App() {
+//   const [isAdmin, setIsAdmin] = useState(false);
 
-  return (
-    <Router>
-       <ScrollToTop />
-      <div className="app-container">
-        <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/AboutOss" element={<AboutOss />} />
-            <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
-            <Route path="/terms" element={<Villkor />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
-  );
-}
+//   return (
+//     <Router>
+//        <ScrollToTop />
+//       <div className="app-container">
+//         <Navbar />
+//         <main className="main-content">
+//           <Routes>
+//             <Route path="/" element={<Home />} />
+//             <Route path="/AboutOss" element={<AboutOss />} />
+//             <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+//             <Route path="/terms" element={<Villkor />} />
+//           </Routes>
+//         </main>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
 
-export default App;
+// export default App;
